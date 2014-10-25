687d8198122270183cc8000be8cacaee77a7f146
Schawnn <iperskill@gmail.com>
2014-10-25 21:29:08 +0200

    Test
    Change-Id: I474317e0f93c093c7b1674446b96df4c49b8dce3

--- src/fr/schawnndev/Main.java
+++ src/fr/schawnndev/Main.java
@@ -1,5 +1,17 @@
 package fr.schawnndev;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class Main extends JavaPlugin {
+
+	public void onEnable() {
+
+	}
+
+	public void onDisable() {
+
+	}
+
+	
+	
 }
