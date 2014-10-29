638ff4937a6898dd2502e29aaf72b21ff7fb0132
Schawnn <iperskill@gmail.com>
2014-10-29 03:07:14 +0100

    Ajout d'events et de commandes

--- src/fr/schawnndev/Events.java
+++ src/fr/schawnndev/Events.java
@@ -1,11 +1,15 @@
 package fr.schawnndev;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 import fr.schawnndev.utils.Checkpoints;
@@ -16,46 +20,65 @@ public class Events implements Listener {
 	Main pl;
 	Vie vie = null;
 	Checkpoints check = null;
+
 	public Events(Main plugin) {
 		pl = plugin;
 	}
+
+/*	@EventHandler
+	public void onPlayerStep(PlayerInteractEvent e) {
+		if (e.getAction().equals(Action.PHYSICAL)) {
+			Player p = e.getPlayer();
+			if (e.getClickedBlock().getType().equals(Material.GOLD_PLATE)) {
+				e.getPlayer().sendMessage(
+						Main.getInstance().starter(
+								"Tu as marché sur une gold plate!"));
+				e.getPlayer().sendMessage(
+								"Tu as marché sur une gold plate!");
+			
+			
+			}
+
+		}
+	} */
 
 	@EventHandler
 	public void onPlayerMove(PlayerMoveEvent e) {
 		Block block = e.getTo().getBlock();
 		if (e.getTo().getBlock().getRelative(BlockFace.DOWN).getType() == Material.GOLD_PLATE) {
-
-			this.check = new Checkpoints(e.getPlayer());
-			this.vie = new Vie(e.getPlayer());
-			
-			if (e.getTo() == Main.getInstance().getStartLoc()) {
-				check.addCheckpoint();
-				vie.addPlayer();
-				Main.playerInJump.add(e.getPlayer());
-				e.getPlayer().sendMessage(Main.getInstance().starter("Bienvenue au jump et bonne chance !"));
-			}
-			
-			if (Main.playerInJump.contains(e.getPlayer())) {
-				if (e.getTo() == Main.getInstance().getCheckPointLocById(1)) {
-					vie.addVie(5);
-					check.addCheckpoint();
-					e.getPlayer().sendMessage(Main.getInstance().starter("Tu as "));
-				} else if (e.getTo() == Main.getInstance()
-						.getCheckPointLocById(check.getCheckpoint())) {
-					vie.addVie(3);
-					check.addCheckpoint();
-					e.getPlayer().sendMessage(Main.getInstance().starter("Bienvenue au jump et bonne chance !"));
-				} else if (e.getTo() == Main.getInstance().getFinishLoc()) {
-					//Partie messages
-					e.getPlayer().sendMessage(Main.getInstance().starter("Bravo tu as réussi le jump!"));
-					Bukkit.broadcastMessage(Main.getInstance().starter(e.getPlayer().getName() + " a réussi le jump avec §4" + vie.get() + " vies §6!"));
-					
-					//Partie remove
-					Main.playerInJump.remove(e.getPlayer());
-					this.vie.removePlayer();
-					this.check.removePlayer();
-				}
-			}
+			e.getPlayer().sendMessage(
+					Main.getInstance().starter(
+							"Tu as marché sur une gold plate!"));
+			/*
+			 * this.check = new Checkpoints(e.getPlayer()); this.vie = new
+			 * Vie(e.getPlayer());
+			 * 
+			 * if (e.getTo() == Main.getInstance().getStartLoc()) {
+			 * check.addCheckpoint(); vie.addPlayer();
+			 * Main.playerInJump.add(e.getPlayer());
+			 * e.getPlayer().sendMessage(Main
+			 * .getInstance().starter("Bienvenue au jump et bonne chance !")); }
+			 * 
+			 * if (Main.playerInJump.contains(e.getPlayer())) { if (e.getTo() ==
+			 * Main.getInstance().getCheckPointLocById(1, e.getPlayer())) {
+			 * vie.addVie(5); check.addCheckpoint();
+			 * e.getPlayer().sendMessage(Main.getInstance().starter("Tu as "));
+			 * } else if (e.getTo() == Main.getInstance()
+			 * .getCheckPointLocById(check.getCheckpoint(), e.getPlayer())) {
+			 * vie.addVie(3); check.addCheckpoint();
+			 * e.getPlayer().sendMessage(Main
+			 * .getInstance().starter("Bienvenue au jump et bonne chance !")); }
+			 * else if (e.getTo() == Main.getInstance().getFinishLoc()) {
+			 * //Partie messages
+			 * e.getPlayer().sendMessage(Main.getInstance().starter
+			 * ("Bravo tu as réussi le jump!"));
+			 * Bukkit.broadcastMessage(Main.getInstance
+			 * ().starter(e.getPlayer().getName() + " a réussi le jump avec §4"
+			 * + vie.get() + " vies §6!"));
+			 * 
+			 * //Partie remove Main.playerInJump.remove(e.getPlayer());
+			 * this.vie.removePlayer(); this.check.removePlayer(); } }
+			 */
 		}
 	}
 
--- src/fr/schawnndev/Main.java
+++ src/fr/schawnndev/Main.java
@@ -11,9 +11,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import fr.schawnndev.utils.Vie;
-
-
 public class Main extends JavaPlugin {
 
 	// > = plus grand | < = plus petit ;
@@ -21,7 +18,7 @@ public class Main extends JavaPlugin {
 	public static ArrayList<Player> playerInJump = new ArrayList<Player>();
 
 	public static Main instance = null;
-	
+
 	public int Checkpoints = getConfig().getInt("Checkpoints");
 
 	public void onEnable() {
@@ -35,99 +32,209 @@ public class Main extends JavaPlugin {
 
 	}
 
+	public void setCheckpoint(Player player) {
+		double x = 0, y = 0, z = 0;
+		float pitch = 0, yaw = 0;
+		String world = null;
+		x = player.getLocation().getX();
+		y = player.getLocation().getY();
+		z = player.getLocation().getZ();
+		yaw = player.getLocation().getYaw();
+		pitch = player.getLocation().getPitch();
+		world = player.getLocation().getWorld().getName();
+		addCheckPointConfig(x, y, z, pitch, yaw, world, "", false);
+		player.sendMessage("X: " + x + " Y: " + y + " Z:" + z + " in world: "
+				+ world + " | Id: " + Checkpoints);
+
+	}
+
+	public void setStart(Player player) {
+		double x = 0, y = 0, z = 0;
+		float pitch = 0, yaw = 0;
+		String world = null;
+		x = player.getLocation().getX();
+		y = player.getLocation().getY();
+		z = player.getLocation().getZ();
+		yaw = player.getLocation().getYaw();
+		pitch = player.getLocation().getPitch();
+		world = player.getLocation().getWorld().getName();
+		addCheckPointConfig(x, y, z, pitch, yaw, world, "start", true);
+		player.sendMessage("X: " + x + " Y: " + y + " Z:" + z + " in world: "
+				+ world);
+	}
+
+	public void setFinish(Player player) {
+		double x = 0, y = 0, z = 0;
+		float pitch = 0, yaw = 0;
+		String world = null;
+		x = player.getLocation().getX();
+		y = player.getLocation().getY();
+		z = player.getLocation().getZ();
+		yaw = player.getLocation().getYaw();
+		pitch = player.getLocation().getPitch();
+		world = player.getLocation().getWorld().getName();
+		addCheckPointConfig(x, y, z, pitch, yaw, world, "finish", true);
+		player.sendMessage("X: " + x + " Y: " + y + " Z:" + z + " in world: "
+				+ world);
+
+	}
+
+	public void sendHelp(Player player) {
+		player.sendMessage("§3--- §eJUMP HELP §3---");
+		player.sendMessage("§3/jump create checkpoint");
+		player.sendMessage("§3/jump remove checkpoint <id>");
+		player.sendMessage("§3/jump list checkpoint");
+		player.sendMessage("§3/jump set start");
+		player.sendMessage("§3/jump set finish");
+		player.sendMessage("§3/jump tp checkpoint <id>");
+	}
+
 	public boolean onCommand(CommandSender sender, Command cmd, String label,
 			String[] args) {
 
 		Player player = (Player) sender;
-
+		if(player.isOp()){
 		if (label.equalsIgnoreCase("jump")) {
-			if (args[0].equalsIgnoreCase("create")) {
-				if (args[1].equalsIgnoreCase("checkpoint")) {
-					double x = 0, y = 0, z = 0;
-					float pitch = 0, yaw = 0;
-					String world = null;
-					x = player.getLocation().getX();
-					y = player.getLocation().getY();
-					z = player.getLocation().getZ();
-					yaw = player.getLocation().getYaw();
-					pitch = player.getLocation().getPitch();
-					world = player.getLocation().getWorld().toString();
-					addCheckPointConfig(x, y, z, pitch, yaw, world);
-					player.sendMessage("X: " + x + " Y: " + y + " Z:" + z
-							+ " in world: " + world);
-					if (args.length > 2) {
-						player.sendMessage("§cTrop d'arguments!");
+
+			if (sender instanceof Player) {
+				
+				if(args.length == 0){
+					if (!player.isOp()) {
+						player.sendMessage(starter("§cTu n'es pas op!"));
+					} else {
+						sendHelp(player);
 					}
 					return true;
 				}
-				/*
-				 * Pas encore* if(args.length > 2){
-				 * player.sendMessage("§cTrop d'arguments!"); }
-				 */
-				if (args.length == 1) {
-					player.sendMessage("§cIl manque des args.");
-				}
-				return true;
-			}
-			if (args[0].equalsIgnoreCase("remove")) {
-				player.sendMessage("Le checkpoint à été remove");
-				return true;
-			}
-			if (args[0].equalsIgnoreCase("list")) {
-				player.sendMessage("- 1 x:1 y:48 z:-21");
-				return true;
-			}
-			if (args[0].equalsIgnoreCase("set")) {
-				player.sendMessage("Le checkpoint à été set");
-				return true;
-			}
-			if (args[0].equalsIgnoreCase("tp")) {
-				if(args.length == 2){
-					int  u= 0;
-					try {
-						String s = args[1];
-						 u = Integer.parseInt(s);
-					} catch (NumberFormatException e) {
-						e.printStackTrace();
+
+		//		if (args.length == 1) {
+					if (args[0].equalsIgnoreCase("create")) {
+						if (args.length == 1) {
+							if (!player.isOp()) {
+								player.sendMessage(starter("§cTu n'es pas op!"));
+							} else {
+								sendHelp(player);
+							}
+							return true;
+						}
+						
+						if (args.length > 3) {
+							if (!player.isOp()) {
+								player.sendMessage(starter("§cTu n'es pas op!"));
+							} else {
+								sendHelp(player);
+							}
+							return true;
+						}
+							if (args[1].equalsIgnoreCase("checkpoint")) {
+								if(args.length == 2){
+								setCheckpoint(player);
+								} else {
+									if (!player.isOp()) {
+										player.sendMessage(starter("§cTu n'es pas op!"));
+									} else {
+										player.sendMessage(starter("§cIl y a trop d'args!"));
+									}
+								}
+								return true;
+							}
+
+							if (args[1].equalsIgnoreCase("start")) {
+								if(args.length == 2){
+								setStart(player);
+								} else {
+									if (!player.isOp()) {
+										player.sendMessage(starter("§cTu n'es pas op!"));
+									} else {
+										player.sendMessage(starter("§cIl y a trop d'args!"));
+									}
+								}
+								return true;
+							}
+
+							if (args[1].equalsIgnoreCase("finish")) {
+								if(args.length == 2){
+								setFinish(player);
+							} else {
+								if (!player.isOp()) {
+									player.sendMessage(starter("§cTu n'es pas op!"));
+								} else {
+									player.sendMessage(starter("§cIl y a trop d'args!"));
+								}
+							}
+								return true;
+							}
+							
+							if (args.length == 2) {
+								if (!player.isOp()) {
+									player.sendMessage(starter("§cTu n'es pas op!"));
+								} else {
+									sendHelp(player);
+								}
+								return true;
+							}
+							
+
+						return true;
 					}
-		
-					player.teleport(getCheckPointLocById(u));
-				}
-				if(args.length == 1){
-					player.sendMessage("§cIl manque une id: /jump tp <id>");
-				}
-				if(args.length > 2){
-					player.sendMessage("§cTrop d'arguments!");
-				}
-				return true;
-			}
+					if (args[0].equalsIgnoreCase("remove")) {
+						player.sendMessage("Le checkpoint à été remove");
+						return true;
+					}
+					if (args[0].equalsIgnoreCase("list")) {
+						player.sendMessage("- 1 x:1 y:48 z:-21");
+						return true;
+					}
+					if (args[0].equalsIgnoreCase("set")) {
+						player.sendMessage("Le checkpoint à été set");
+						return true;
+					}
+					if (args[0].equalsIgnoreCase("tp")) {
+						if (args.length == 2) {
+							int u = 0;
+							try {
+								String s = args[1];
+								u = Integer.parseInt(s);
+							} catch (NumberFormatException e) {
+								player.sendMessage(starter("§cCe n'est pas une id!"));
+								return true;
+							}
 
-			if (args.length == 0) {
-				if (!player.isOp()) {
-					player.sendMessage("§cTu n'es pas op!");
-				} else {
-					player.sendMessage("§3--- §eJUMP HELP §3---");
-					player.sendMessage("§3/jump create checkpoint");
-					player.sendMessage("§3/jump remove checkpoint <id>");
-					player.sendMessage("§3/jump list checkpoint");
-					player.sendMessage("§3/jump set start");
-					player.sendMessage("§3/jump set finish");
-					player.sendMessage("§3/jump tp checkpoint <id>");
-				}
+							player.teleport(getCheckPointLocById(u, player));
+						}
+						if (args.length == 1) {
+							player.sendMessage(starter("§cIl manque une id: /jump tp <id>"));
+						}
+						if (args.length > 2) {
+							player.sendMessage(starter("§cTrop d'arguments!"));
+						}
+						return true;
+					}
+
+					// FIN cmd:jump
+					return true;
+			//	} else {
+			//		if (!player.isOp()) {
+			//			player.sendMessage("§cTu n'es pas op!");
+			//		} else {
+			//			sendHelp(player);
+			//		}
+			//	}
 			}
-			// FIN cmd:jump
 			return true;
+			}
+		} else {
+			player.sendMessage(starter("§cTu n'es pas op!"));
 		}
-
 		return false;
 	}
-	
-	public static Main getInstance(){
+
+	public static Main getInstance() {
 		return instance;
 	}
 
-	public Location getCheckPointLocById(int id) {
-		if (!(id > Checkpoints) && (id >=1)) {
+	public Location getCheckPointLocById(int id, Player player) {
+		if (!(id > Checkpoints) && (id >= 1)) {
 			double x = 0, y = 0, z = 0;
 			float pitch = 0, yaw = 0;
 			String world = null;
@@ -139,24 +246,26 @@ public class Main extends JavaPlugin {
 			world = getConfig().getString("Checkpoint." + id + ".world");
 			World w = Bukkit.getWorld(world);
 			return new Location(w, x, y, z, yaw, pitch);
+		} else {
+			player.sendMessage(starter("§cL'id n'est pas enregistrée!"));
+			return player.getLocation();
 		}
-		return null;
 	}
-	
+
 	public Location getStartLoc() {
-			double x = 0, y = 0, z = 0;
-			float pitch = 0, yaw = 0;
-			String world = null;
-			x = getConfig().getDouble("Checkpoint." + "start" + ".x");
-			y = getConfig().getDouble("Checkpoint." + "start" + ".y");
-			z = getConfig().getDouble("Checkpoint." + "start" + ".z");
-			pitch = getConfig().getLong("Checkpoint." + "start" + ".pitch");
-			yaw = getConfig().getLong("Checkpoint." + "start" + ".yaw");
-			world = getConfig().getString("Checkpoint." + "start" + ".world");
-			World w = Bukkit.getWorld(world);
-			return new Location(w, x, y, z, yaw, pitch);
+		double x = 0, y = 0, z = 0;
+		float pitch = 0, yaw = 0;
+		String world = null;
+		x = getConfig().getDouble("Checkpoint." + "start" + ".x");
+		y = getConfig().getDouble("Checkpoint." + "start" + ".y");
+		z = getConfig().getDouble("Checkpoint." + "start" + ".z");
+		pitch = getConfig().getLong("Checkpoint." + "start" + ".pitch");
+		yaw = getConfig().getLong("Checkpoint." + "start" + ".yaw");
+		world = getConfig().getString("Checkpoint." + "start" + ".world");
+		World w = Bukkit.getWorld(world);
+		return new Location(w, x, y, z, yaw, pitch);
 	}
-	
+
 	public Location getFinishLoc() {
 		double x = 0, y = 0, z = 0;
 		float pitch = 0, yaw = 0;
@@ -169,17 +278,26 @@ public class Main extends JavaPlugin {
 		world = getConfig().getString("Checkpoint." + "finish" + ".world");
 		World w = Bukkit.getWorld(world);
 		return new Location(w, x, y, z, yaw, pitch);
-}
+	}
 
 	private void addCheckPointConfig(double x, double y, double z, float pitch,
-			float yaw, String world) {
-		int i = getNextAndSetInt();
-		getConfig().set("Checkpoint." + i + ".x", x);
-		getConfig().set("Checkpoint." + i + ".y", y);
-		getConfig().set("Checkpoint." + i + ".z", z);
-		getConfig().set("Checkpoint." + i + ".pitch", pitch);
-		getConfig().set("Checkpoint." + i + ".yaw", yaw);
-		getConfig().set("Checkpoint." + i + ".world", world);
+			float yaw, String world, String name, boolean isName) {
+		if (!isName) {
+			int i = getNextAndSetInt();
+			getConfig().set("Checkpoint." + i + ".x", x);
+			getConfig().set("Checkpoint." + i + ".y", y);
+			getConfig().set("Checkpoint." + i + ".z", z);
+			getConfig().set("Checkpoint." + i + ".pitch", pitch);
+			getConfig().set("Checkpoint." + i + ".yaw", yaw);
+			getConfig().set("Checkpoint." + i + ".world", world);
+		} else {
+			getConfig().set("Checkpoint." + name + ".x", x);
+			getConfig().set("Checkpoint." + name + ".y", y);
+			getConfig().set("Checkpoint." + name + ".z", z);
+			getConfig().set("Checkpoint." + name + ".pitch", pitch);
+			getConfig().set("Checkpoint." + name + ".yaw", yaw);
+			getConfig().set("Checkpoint." + name + ".world", world);
+		}
 		saveConfig();
 	}
 
@@ -206,7 +324,7 @@ public class Main extends JavaPlugin {
 			return true;
 		return false;
 	}
-	
+
 	public String starter(String msg) {
 		return "§7[" + "§cJump" + "§7] §6" + msg;
 	}
